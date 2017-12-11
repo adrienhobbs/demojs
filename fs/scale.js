ce988fdfb6616a380c20067a30b2974239d9fb4c
Adrien Hobbs <adrien.hobbs@gmail.com>
2017-12-11 09:21:15 -0500

    updates

--- fs/scale.js
+++ fs/scale.js
@@ -2,17 +2,43 @@ load('api_config.js');
 load('api_gpio.js');
 load('api_uart.js');
 load('api_aws.js');
+load('api_neopixel.js');
+
+let pin = 13, numPixels = 8, colorOrder = NeoPixel.GRB;
+let capacity = 8;
 
 let Scale = {
   tareCharacter: 'y',
   readingCharacter: 'z',
   uartNo: 1,
+  taring: false,
+  capacity: 0,
+  initStatusLED: function() {
+    this.strip = NeoPixel.create(pin, numPixels, colorOrder);
+    this.strip.clear();
+    this.strip.show();
+  },
+  updateStatusLED: function(currentWeight) {
+    let pixelsToShow = (currentWeight / capacity) * numPixels;
+    this.strip.clear();
+    for (let i = 0; i <= pixelsToShow; i++) {
+      if (pixelsToShow >= numPixels / 2) {
+        this.strip.setPixel(i, 0, 255, 0);  
+      } else if (pixelsToShow <= numPixels / 2 && currentWeight / capacity > 0.25) {
+        this.strip.setPixel(i, 255, 255, 0);  
+      } else {
+        this.strip.setPixel(i, 255, 0, 0);
+      }
+    }
+    this.strip.show();
+  },
   init: function () {
     this.topic = '/devices/' + Cfg.get('device.id') + '/events';
     this.setupUART();
     this.listenForScaleUpdates();
     this.setShadowHandlers();
     this.handleBtnPress();
+    this.initStatusLED();
   },
   setupUART: function () {
     UART.setConfig(this.uartNo, {
@@ -27,13 +53,18 @@ let Scale = {
     UART.setRxEnabled(this.uartNo, true);
   },
   listenForScaleUpdates: function () {
+    let updateLED = this.updateStatusLED;
     UART.setDispatcher(this.uartNo, function (uartNo) {
       let ra = UART.readAvail(uartNo);
       if (ra > 0) {
         let ud = UART.read(uartNo);
-        if (ud.at(0) === 123) {
+        if (Scale.taring && ud === 'TARE_DONE') {
+          Scale.taring = false;
+        }
+        if (ud.at(0) === 123) { // data returned is JSON
           let data = JSON.parse(ud);
-          AWS.Shadow.update(0, { reported: { temperature: data.t, weight: data.w } });
+          AWS.Shadow.update(0, { reported: { temperature: data.t, weight: data.w, capacity: Scale.capacity || 40 } });
+          Scale.updateStatusLED(data.w);
         }
       }
     }, null);
@@ -41,6 +72,7 @@ let Scale = {
   setShadowHandlers: function () {
     AWS.Shadow.setStateHandler(function (data, event, reported, desired) {
       if (event === AWS.Shadow.CONNECTED) {
+        print("YAHH", JSON.stringify(reported));
         Scale.triggerReading();
       } else if (event === AWS.Shadow.UPDATE_DELTA) {
         if (desired.tare) {
@@ -51,8 +83,12 @@ let Scale = {
     }, null);
   },
   handleBtnPress: function () {
-    GPIO.set_button_handler(Cfg.get('pins.button'), GPIO.PULL_UP, GPIO.INT_EDGE_NEG, 200, function () {
-      Scale.tare();
+    GPIO.set_button_handler(34, GPIO.PULL_DOWN, GPIO.INT_EDGE_NEG, 200, function(x) {
+      if (!Scale.taring) {
+        Scale.taring = true;
+        Scale.tare();
+        print('Button press, pin: ', x); 
+      }
     }, null);
   },
   tare: function () {
